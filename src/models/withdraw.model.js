70787694b164b2ecf25e4027402904849d522b7d
irvan <irvanyusufcahyadi4@gmail.com>
2025-08-05 15:24:41 +0700

    feat: create withdraw.model.js and withdraw.validation.js

---
+++ src/models/withdraw.model.js
@@ -0,0 +1,70 @@
+const { model, Schema } = require("mongoose");
+
+const withdrawSchema = new Schema(
+  {
+    owner_id: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: [1, "Minimum withdraw is 1"],
+    },
+    admin_fee: {
+      type: Number,
+      required: true,
+      min: [1, "Minimum admin fee is 1"],
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "completed", "rejected", "cancelled"],
+      default: "pending",
+    },
+    payment_method: {
+      type: String,
+      enum: ["bank_transfer", "e_wallet", "cash"],
+      default: "bank_transfer",
+      required: true,
+    },
+    bank_name: {
+      type: String,
+      required: true,
+    },
+    account_name: {
+      type: String,
+      required: true,
+    },
+    account_number: {
+      type: String,
+      required: true,
+    },
+    notes: {
+      type: String,
+      default: "",
+    },
+    created_at: {
+      type: Date,
+    },
+    approved_at: {
+      type: Date,
+    },
+    rejected_at: {
+      type: Date,
+    },
+    cancelled_at: {
+      type: Date,
+    },
+    finished_at: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Withdraw = model("Withdraw", withdrawSchema);
+
+module.exports = { Withdraw };
---
+++ src/validation/withdraw.validation.js
@@ -0,0 +1,12 @@
+const Joi = require("joi");
+
+const createWithdraw = Joi.object({
+  amount: Joi.number().min(1).required(),
+  admin_fee: Joi.number().min(1).required(),
+  bank_name: Joi.string().trim().required(),
+  account_name: Joi.string().trim().required(),
+  account_number: Joi.string().trim().required(),
+  notes: Joi.string().optional(),
+});
+
+module.exports = { createWithdraw };

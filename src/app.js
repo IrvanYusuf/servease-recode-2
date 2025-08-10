4877538b5c6a96092081cbfebe3e615fd4a23c53
irvan <irvanyusufcahyadi4@gmail.com>
2025-08-10 18:44:45 +0700

    add morgan dev for monitor api routes http/https, adding error handler middleware

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 require("module-alias/register");
 const express = require("express");
+const morgan = require("morgan");
 const connectDb = require("@/database/db.js");
 const apiEndpoints = require("@/routes/index.js");
 const cors = require("cors");
 const multerErrorHandler = require("@/errors/multerError.js");
+const errorHandler = require("@/errors/errorHandler");
 
 // dotenv.config();
 
 const app = express();
 
 app.use(cors());
+app.use(morgan("dev"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,6 +22,7 @@ app.get("/", (req, res) => {
   res.send("halo");
 });
 app.use("/api", apiEndpoints);
+app.use(errorHandler);
 app.use(multerErrorHandler);
 
 module.exports = app;

3458298824405f896cc3e688f2100374a94c96dc
irvan <irvanyusufcahyadi4@gmail.com>
2025-08-10 18:41:51 +0700

    remove try catch, use centralized error middleware

--- src/controllers/v1/video.controller.js
+++ src/controllers/v1/video.controller.js
@@ -5,72 +5,49 @@ const { uploadToCloudinary } = require("@/utils/uploadToCloudinary");
 const redisClient = require("@/config/redis");
 class VideoController {
   static index = async (req, res) => {
-    try {
-      const cacheKey = "video-tutorials";
-      const cached = await redisClient.get(cacheKey);
-      if (cached) {
-        console.log(
-          "🍕 Serving data video tutorials from Redis with key:",
-          cacheKey
-        );
-        return ApiResponse.successResponse(
-          res,
-          "success get datas",
-          JSON.parse(cached),
-          null,
-          StatusCodes.OK
-        );
-      }
-      const videos = await Video.find();
-      await redisClient.setEx(cacheKey, 18000, JSON.stringify(videos)); // 1 jam
+    const cacheKey = "video-tutorials";
+    const cached = await redisClient.get(cacheKey);
+    if (cached) {
+      console.log(
+        "🍕 Serving data video tutorials from Redis with key:",
+        cacheKey
+      );
       return ApiResponse.successResponse(
         res,
         "success get datas",
-        videos,
+        JSON.parse(cached),
         null,
         StatusCodes.OK
       );
-    } catch (error) {
-      console.error(error);
-      return ApiResponse.errorResponse(res, "Internal server error", {
-        server: error.message,
-      });
     }
+    const videos = await Video.find();
+    await redisClient.setEx(cacheKey, 18000, JSON.stringify(videos)); // 1 jam
+    return ApiResponse.successResponse(
+      res,
+      "success get datas",
+      videos,
+      null,
+      StatusCodes.OK
+    );
   };
 
   static store = async (req, res) => {
-    try {
-      if (!req.file) {
-        return ApiResponse.errorResponse(
-          res,
-          "File thumbnail wajib diupload",
-          {},
-          StatusCodes.BAD_REQUEST
-        );
-      }
+    const thumbnailUrl = await uploadToCloudinary({
+      buffer: req.file.buffer,
+    });
 
-      const thumbnailUrl = await uploadToCloudinary({
-        buffer: req.file.buffer,
-      });
-
-      const video = await Video.create({
-        name: req.body.name,
-        url_thumbnail: thumbnailUrl,
-        url_video: req.body.url_video,
-      });
-      return ApiResponse.successResponse(
-        res,
-        "success create video",
-        video,
-        null,
-        StatusCodes.CREATED
-      );
-    } catch (error) {
-      console.error(error);
-      return ApiResponse.errorResponse(res, "Internal server error", {
-        server: error.message,
-      });
-    }
+    const video = await Video.create({
+      name: req.body.name,
+      url_thumbnail: thumbnailUrl,
+      url_video: req.body.url_video,
+    });
+    return ApiResponse.successResponse(
+      res,
+      "success create video",
+      video,
+      null,
+      StatusCodes.CREATED
+    );
   };
 }
 

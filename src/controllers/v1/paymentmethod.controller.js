eb5f54f65872fd7d17f416951b3fe1eca385153e
irvan <irvanyusufcahyadi4@gmail.com>
2025-08-10 18:37:46 +0700

    update paymentmethod.controller.js and paymentmethod.route.js

--- src/controllers/v1/paymentmethod.controller.js
+++ src/controllers/v1/paymentmethod.controller.js
@@ -7,83 +7,52 @@ const { uploadToCloudinary } = require("@/utils/uploadToCloudinary");
 const { PaymentMethod } = require("@/models/paymentMethod.model");
 class PaymentMethodController {
   static index = async (req, res) => {
-    try {
-      const data = await PaymentMethod.find();
-      return ApiResponse.successResponse(
-        res,
-        "success get datas",
-        data,
-        null,
-        StatusCodes.OK
-      );
-    } catch (error) {
-      console.error(error);
-      return ApiResponse.errorResponse(res, "Internal server error", {
-        server: error.message,
-      });
-    }
+    const data = await PaymentMethod.find();
+    return ApiResponse.successResponse(
+      res,
+      "success get datas",
+      data,
+      null,
+      StatusCodes.OK
+    );
   };
 
   static store = async (req, res) => {
-    try {
-      const body = req.body;
-      const result = createPaymentMethodSchema.validate(body, {
-        abortEarly: false,
-      });
-      if (result.error) {
-        const errors = result.error.details.map((err) => ({
-          field: err.context.key,
-          message: err.message,
-        }));
+    const data = req.validated;
 
+    let bankLogoUrl = null;
+    if (data.type === "bank_transfer") {
+      if (!req?.file?.buffer) {
         return ApiResponse.errorResponse(
           res,
-          "Validation failed",
-          errors,
+          "Bank logo file is required",
+          null,
           StatusCodes.BAD_REQUEST
         );
       }
-      const data = result.value;
 
-      let bankLogoUrl = null;
-      if (data.type === "bank_transfer") {
-        if (!req?.file?.buffer) {
-          return ApiResponse.errorResponse(
-            res,
-            "Bank logo file is required",
-            null,
-            StatusCodes.BAD_REQUEST
-          );
-        }
-
-        bankLogoUrl = await uploadToCloudinary({
-          buffer: req.file.buffer,
-        });
-      }
-
-      const paymentMethodCreate = await PaymentMethod.create({
-        name: data.name,
-        type: data.type,
-        description: data.description,
-        bank_name: data.bank_name,
-        bank_logo: bankLogoUrl,
-        account_holder: data.account_holder,
-        account_number: data.account_number,
-      });
-
-      return ApiResponse.successResponse(
-        res,
-        "success create payment method",
-        paymentMethodCreate,
-        null,
-        StatusCodes.CREATED
-      );
-    } catch (error) {
-      console.error(error);
-      return ApiResponse.errorResponse(res, "Internal server error", {
-        server: error.message,
+      bankLogoUrl = await uploadToCloudinary({
+        buffer: req.file.buffer,
       });
     }
+
+    const paymentMethodCreate = await PaymentMethod.create({
+      name: data.name,
+      type: data.type,
+      description: data.description,
+      bank_name: data.bank_name,
+      bank_logo: bankLogoUrl,
+      account_holder: data.account_holder,
+      account_number: data.account_number,
+    });
+
+    return ApiResponse.successResponse(
+      res,
+      "success create payment method",
+      paymentMethodCreate,
+      null,
+      StatusCodes.CREATED
+    );
   };
 }
 
--- src/routes/v1/paymentmethod.route.js
+++ src/routes/v1/paymentmethod.route.js
@@ -1,16 +1,23 @@
 const upload = require("@/config/multer");
 const PaymentMethodController = require("@/controllers/v1/paymentmethod.controller");
 const authMiddleware = require("@/middlewares/auth.middleware");
+const validateMiddleware = require("@/middlewares/validate.middleware");
+const {
+  createPaymentMethodSchema,
+} = require("@/validation/paymentMethod.validation");
 const express = require("express");
 
 const router = express.Router();
 
+// public route
 router.get("/", PaymentMethodController.index);
 
+// private route
+router.use(authMiddleware);
 router.post(
   "/",
   upload.single("bank_logo"),
-  authMiddleware,
+  validateMiddleware(createPaymentMethodSchema),
   PaymentMethodController.store
 );
 

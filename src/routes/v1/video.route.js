8cb6c3f3a243e6592cdff0e4f521d94ff88f9016
irvan <irvanyusufcahyadi4@gmail.com>
2025-08-10 18:43:39 +0700

    update video.route.js

--- src/routes/v1/video.route.js
+++ src/routes/v1/video.route.js
@@ -1,10 +1,16 @@
 const upload = require("@/config/multer");
 const VideoController = require("@/controllers/v1/video.controller");
+const checkFilePresenceHandler = require("@/errors/checkFilePresenceHandler");
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", VideoController.index);
-router.post("/", upload.single("thumbnail"), VideoController.store);
+router.post(
+  "/",
+  upload.single("thumbnail"),
+  checkFilePresenceHandler(["thumbnail"]),
+  VideoController.store
+);
 
 module.exports = router;

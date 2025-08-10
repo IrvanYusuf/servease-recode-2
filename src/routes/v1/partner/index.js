f53587ff33d61f4c585b7b502ea106aa30617922
irvan <irvanyusufcahyadi4@gmail.com>
2025-08-10 18:44:06 +0700

    add earnings routes

--- src/routes/v1/partner/index.js
+++ src/routes/v1/partner/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const earningRoutes = require("./earning.route.js");
 const reviewRoutes = require("./review.route.js");
 const bookingPartnerRoutes = require("./booking.route.js");
 const servicePartnerRoutes = require("./service.route.js");
@@ -9,4 +10,5 @@ router.use("/bookings", bookingPartnerRoutes);
 router.use("/services", servicePartnerRoutes);
 
 router.use("/reviews", reviewRoutes);
+router.use("/earnings", earningRoutes);
 module.exports = router;
